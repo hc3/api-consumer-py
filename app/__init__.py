27d152572af84c5c89411d640614c9b40208d5a6
hc3 <eliel.floyd@bol.com.br>
2019-07-23 16:20:33 -0300

    evoluindo projeto.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from .model import configure as config_db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
+    config_db(app)
 
+    Migrate(app, app.db)
+
+    
+    #'mysql://root:rooot@localhost/db_rn'
     return app
---
+++ app/serealizer.py
@@ -0,0 +1,10 @@
+from flask_marshmallow import Marshmallow
+
+ma = Marshmallow()
+
+def configure(app):
+    ma.init_app(app)
+
+
+class BookSchema(ma.ModelSchema):
+    
